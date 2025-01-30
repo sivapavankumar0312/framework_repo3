15b65336ab27633f691635d0ee730b85e226c3e9
home <home@DESKTOP-LBNV72U>
2025-01-30 17:01:31 +0530

    commited

---
+++ SeleniumProject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,14 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createorgTest() {
+		System.out.println("execute createorgTest");
+	}
+	@Test
+	public void modifyorgTest() {
+		System.out.println("execute modifyorgTest");
+	}
+}
